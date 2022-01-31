960330ff0ab2dd84d76ad383c5160f9cb58056c3
firdavsxon <akilovfs@gmail.com>
2022-01-30 18:54:12 -0800

    adding users feature to project

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+	form = UserCreationForm()
+	return render(request, 'users/register.html', {'form': form})
+
+
+def login(request):
+	form = UserCreationForm()
+	return render(request, 'users/login.html', {'form': form})
+
