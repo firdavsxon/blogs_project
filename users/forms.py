c345f129b59fbb35a1af36de36d24234cfd1ad75
firdavsxon <akilovfs@gmail.com>
2022-02-03 08:15:32 -0800

    ddidng forms to projects

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+class UserRegisterForm(UserCreationForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserAuthenticationForm(AuthenticationForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ('username', 'email')
+
