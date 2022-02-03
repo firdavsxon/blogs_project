c33e9cf8e069303ddc63884e112bf46144d89ffb
firdavsxon <akilovfs@gmail.com>
2022-02-03 08:17:33 -0800

    update in views

--- users/views.py
+++ users/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserAuthenticationForm
 
 
 def register(request):
-	form = UserCreationForm()
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user_name = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {user_name}!')
+			return redirect('blog-home')
+	else:
+		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
 
 def login(request):
-	form = UserCreationForm()
+	form = UserAuthenticationForm(request.POST)
 	return render(request, 'users/login.html', {'form': form})
 
